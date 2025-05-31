b59ac44e251fb8b9a33c11a9920b27918c90157f
Bhavesh Kanojia <bhaveshkanojia10@gmail.com>
2025-05-31 19:17:51 +0530

    Migrated to cloud from local server

--- init/init.js
+++ init/init.js
@@ -7,17 +7,17 @@ main()
   .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/landsum");
+  await mongoose.connect(process.env.MONGODB_URI);
 }
 
 const initDB = () => {
   Listing.deleteMany({})
     .then((res) => console.log(res))
     .catch((err) => console.log(err));
-    initData.data = initData.data.map((obj)=>({
-      ...obj,
-      owner : "67bdcdee31a8ff980bf122bb",
-    }))
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "67bdcdee31a8ff980bf122bb",
+  }));
   Listing.insertMany(initData.data)
     .then((res) => console.log("Database is Initialized"))
     .catch((err) => console.log(err));
--- routes/listing.js
+++ routes/listing.js
@@ -1,43 +1,42 @@
 const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
-const {isLoggedIn,isOwner,validateListing} = require("../middleware.js");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
 const multer = require("multer");
-const {storage} = require("../cloudConfig.js");
+const { storage } = require("../config/cloudConfig.js");
 const upload = multer({ storage });
 
-router.route("/")
-.get(
-  wrapAsync(listingController.index)
-)
-.post(
-  isLoggedIn,
-  upload.single("listing[image]"),
-  validateListing,
-  wrapAsync(listingController.createListing)
-);
+router
+  .route("/")
+  .get(wrapAsync(listingController.index))
+  .post(
+    isLoggedIn,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.createListing)
+  );
 
-  
 // New Get Route
-router.get("/new",isLoggedIn,listingController.renderNewForm);
+router.get("/new", isLoggedIn, listingController.renderNewForm);
 
-router.route("/:id")
-.get(
-  wrapAsync(listingController.showListing)
-)
-.put(
-  isLoggedIn,isOwner,upload.single("listing[image]"),validateListing,
-  wrapAsync(listingController.updateListing)
-)
-.delete(
-  isLoggedIn,isOwner,
-  wrapAsync(listingController.destroyListing)
-);
+router
+  .route("/:id")
+  .get(wrapAsync(listingController.showListing))
+  .put(
+    isLoggedIn,
+    isOwner,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.updateListing)
+  )
+  .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
 // Edit get Route
 router.get(
-  "/:id/edit",isLoggedIn,isOwner,
+  "/:id/edit",
+  isLoggedIn,
+  isOwner,
   wrapAsync(listingController.renderEditForm)
 );
 
